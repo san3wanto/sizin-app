5e4a354c1f305c3b1124142fd5ac0bbb490761a1
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-02 13:11:24 +0800

    Modal hapus data user

--- src/components/IzinList.jsx
+++ src/components/IzinList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { Container, Table, Button } from "react-bootstrap";
+import { Container, Table, Button, Modal } from "react-bootstrap";
 import "bootstrap";
 import "../app.css";
 
