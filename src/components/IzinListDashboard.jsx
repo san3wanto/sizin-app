d60f4fb5b65a5afbcbe959b9487a131d8cc27418
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-03 14:21:28 +0800

    tombol batal disabled

--- src/components/IzinListDashboard.jsx
+++ src/components/IzinListDashboard.jsx
@@ -79,19 +79,26 @@ const IzinList = () => {
         <Modal.Body>{`${izin[izin?.length - 1]?.ket}`}</Modal.Body>
         <Modal.Footer>
           {loading === true ? (
-            <Button variant="primary" disabled>
-              <Spinner as="span" animation="grow" size="sm" role="status" aria-hidden="true" />
-              {console.log(loading)}
-              Memproses...
-            </Button>
+            <div>
+              <Button variant="primary" disabled>
+                <Spinner as="span" animation="grow" size="sm" role="status" aria-hidden="true" />
+                {console.log(loading)}
+                Memproses...
+              </Button>
+              <Button variant="danger" disabled>
+                Batal
+              </Button>
+            </div>
           ) : (
-            <Button variant="primary" onClick={() => doubleUp(user && user.uuid, izin[izin?.length - 1]?.uuid)}>
-              Selesaikan
-            </Button>
+            <div>
+              <Button variant="primary" onClick={() => doubleUp(user && user.uuid, izin[izin?.length - 1]?.uuid)}>
+                Selesaikan
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Batal
+              </Button>
+            </div>
           )}
-          <Button variant="danger" onClick={handleClose}>
-            Batal
-          </Button>
         </Modal.Footer>
       </Modal>
       <div>
