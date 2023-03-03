4a342587f0695f58246d88624c69a8fac183f254
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-03 17:35:01 +0800

    custom page login fixing

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { LoginUser, reset } from "../features/authSlice";
-import { Button, Container, Form, InputGroup, Card, Spinner, Table, Button, Badge, Alert } from "react-bootstrap";
+import { Button, Container, Form, InputGroup, Card, Spinner, Table, Badge, Alert } from "react-bootstrap";
 import logo from "../bps.png";
 
 const Login = () => {
