31f73c97b408a00d267ac3e38637645f0ec46685
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-06 23:30:30 +0800

    tes reducer 4

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { persistor, store } from "./App/store";
+import { persistor, store } from "./app/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 axios.defaults.withCredentials = true;
