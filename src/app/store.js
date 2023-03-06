c26f04895a3b6ec5e05b2fcd424924535a3aa93c
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-07 00:19:21 +0800

    remove redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,26 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit"; //setting awal
-import authSlice from "../features/authSlice"; //setting awal
-
-import storage from "redux-persist/lib/storage";
-import { persistStore, persistReducer } from "redux-persist";
-
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, authSlice);
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    auth: authReducer,
+  },
 });
-
-// setting awal
-// export const store = configureStore({
-//   reducer: {
-//     auth: authReducer,
-//   },
-// });
-//setting awal
-
-export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-// import { store } from "./app/store";
+import { store } from "./app/store";
 import App from "./App";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { persistor, store } from "./app/store";
-import { PersistGate } from "redux-persist/integration/react";
 
 axios.defaults.withCredentials = true;
 // agar setiap request ke server selalu menyertakan kredensialnya, berlaku secara global
@@ -17,9 +15,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
