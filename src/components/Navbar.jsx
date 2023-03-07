8ec57ef63e1e195d0430c0e124363107d037f443
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-07 18:58:51 +0800

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,37 +18,39 @@ const NavB = () => {
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="primary" variant="white" sticky="top">
-      <Container>
-        <Navbar.Brand>
-          <img alt="logo bps palu" src={logo} style={{ width: "200px" }} />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="bg-white size-sm" />
-        <Navbar.Collapse id="responsive-navbar-nav" className="">
-          <Nav className="me-auto"></Nav>
-          <Nav>
-            <Link to={"/dashboard"} className="nav-link text-white" style={{ textDecoration: "none" }}>
-              Dashboard
-            </Link>
-            {user && user.role === "admin" && (
-              <Nav>
-                <Link to={"/users"} className="nav-link text-white" style={{ textDecoration: "none" }}>
-                  Pegawai
-                </Link>
-              </Nav>
-            )}
-            {user && user.role === "admin" && (
-              <Link to={"/izin"} className="nav-link text-white" style={{ textDecoration: "none" }}>
-                Riwayat Izin
+      {user && (
+        <Container>
+          <Navbar.Brand>
+            <img alt="logo bps palu" src={logo} style={{ width: "200px" }} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="bg-white size-sm" />
+          <Navbar.Collapse id="responsive-navbar-nav" className="">
+            <Nav className="me-auto"></Nav>
+            <Nav>
+              <Link to={"/dashboard"} className="nav-link text-white" style={{ textDecoration: "none" }}>
+                Dashboard
               </Link>
-            )}
-            <NavDropdown title={`${user && user.name} (${user && user.role})`} id="collasible-nav-dropdown" className="text-white">
-              <NavDropdown.Item className="" onClick={logout}>
-                Logout
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+              {user && user.role === "admin" && (
+                <Nav>
+                  <Link to={"/users"} className="nav-link text-white" style={{ textDecoration: "none" }}>
+                    Pegawai
+                  </Link>
+                </Nav>
+              )}
+              {user && user.role === "admin" && (
+                <Link to={"/izin"} className="nav-link text-white" style={{ textDecoration: "none" }}>
+                  Riwayat Izin
+                </Link>
+              )}
+              <NavDropdown title={`${user && user.name} (${user && user.role})`} id="collasible-nav-dropdown" className="text-white">
+                <NavDropdown.Item className="" onClick={logout}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      )}
     </Navbar>
   );
 };
