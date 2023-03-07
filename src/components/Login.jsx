cef157cb439b8666eb4048a82d2d5231672c5df7
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-07 21:14:38 +0800

    navbar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -77,7 +77,7 @@ const Login = () => {
   };
 
   return (
-    <Container fluid className="d-flex">
+    <Container fluid>
       <Navbar className="p-0" bg="primary" variant="white" sticky="top">
         <Container>
           <Navbar.Brand>
