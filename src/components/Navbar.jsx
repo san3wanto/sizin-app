a4defbd4e857542c03dbb4af6150f09d7261b5ef
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-07 18:20:54 +0800

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const NavB = () => {
   };
 
   return (
-    <Navbar collapseOnSelect expand="lg" bg="primary" variant="white" fixed="top" className="mb-5">
+    <Navbar collapseOnSelect expand="lg" bg="primary" variant="white" fixed="sticky">
       <Container>
         <Navbar.Brand>
           <img alt="logo bps palu" src={logo} style={{ width: "200px" }} />
