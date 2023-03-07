b3f74b82c2f97b4a8f1bffaee55aada4633ddcad
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-07 19:21:16 +0800

    page login navbar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -78,7 +78,7 @@ const Login = () => {
 
   return (
     <Container>
-      <Navbar bg="primary" variant="white" fixed="sticky">
+      <Navbar expand="lg" bg="primary" variant="white" sticky="top">
         <Container>
           <Navbar.Brand>
             <img alt="logo bps palu" src={logo} style={{ width: "200px" }} />
