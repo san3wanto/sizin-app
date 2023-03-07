a1ed839a50d42aac5b864e3d5c14ed9ba0a7524d
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-07 17:03:12 +0800

    reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 
 export const store = configureStore({
-  reducer: authReducer,
-  devTools: process.env.NODE_ENV !== 'production',
+  reducer: {
+    auth: authReducer,
   },
-);
-
+});
 
