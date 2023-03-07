900effeb8e712fc1ff83f3f9d10e17f6a4d657d5
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-07 21:10:51 +0800

    navbar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -77,7 +77,7 @@ const Login = () => {
   };
 
   return (
-    <div>
+    <Container>
       <Navbar className="p-0" bg="primary" variant="white" sticky="top">
         <Container>
           <Navbar.Brand>
@@ -223,7 +223,7 @@ const Login = () => {
           </Container>
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
