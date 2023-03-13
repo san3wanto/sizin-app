6c2749281b0ac9671b3266125c4760691cc067cb
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-13 19:24:02 +0800

    tab

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -224,15 +224,14 @@ const Login = () => {
                                     <Col>
                                       <strong>Riwayat</strong>
                                     </Col>
-                                    <Col>{!user.izinData[user.izinData.length - 1] ? "Tidak Ada" : `${user.izinData[user.izinData.length - 1].ket}`}</Col>
+                                    <Col>{user.izinData[user.izinData.length - 1].ket}</Col>
                                   </Row>
                                   <hr></hr>
                                   <Row>
                                     <Col>
                                       <strong>Selesai</strong>
                                     </Col>
-                                    <Col>{!user.izinData[user.izinData.length - 1] ? "Tidak Ada" : `${dayjs(user.izinData[user.izinData.length - 1].updatedAt).format("HH:mm - dddd, DD MM YYYY")}`}</Col>
-                                    {/* <Col>{`${dayjs(user.izinData[user.izinData.length - 1].updatedAt).format("HH:mm - dddd, DD MM YYYY")}`}</Col> */}
+                                    <Col>{`${dayjs(user.izinData[user.izinData.length - 1].updatedAt).format("HH:mm - dddd, DD MM YYYY")}`}</Col>
                                   </Row>
                                 </Accordion.Body>
                               </Accordion.Item>
