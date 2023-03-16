6522f51aac7779a2bdb4bde0006540e08e4384a9
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-16 13:50:04 +0800

    tes

--- src/components/FormAddIzin.jsx
+++ src/components/FormAddIzin.jsx
@@ -30,9 +30,9 @@ const FormAddIzin = () => {
       if (error.response) {
         setMsg(error.response.data.msg);
         setPsn(error.response.data.msg);
+        setLoading(false);
       }
     }
-    setLoading(false);
     setMsg("");
   };
 
@@ -49,7 +49,7 @@ const FormAddIzin = () => {
           <FloatingLabel controlId="floatingTextarea" label="Keterangan Izin" className="mb-3 w-100">
             <Form.Control as="textarea" className="input" value={ket} onChange={(e) => setKet(e.target.value)} placeholder="keterangan izin" />
           </FloatingLabel>
-          {loading === false && !msg ? (
+          {loading === false && msg === "" ? (
             <Button type="submit" onClick={() => updateUserStatus(user && user.uuid)} className="Button is-success w-100">
               Simpan
             </Button>
