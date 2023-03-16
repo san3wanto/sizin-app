04284dfb669850daa2afae6b10c111c9af9a163a
Sandi Triwanto <sandimuhammad7@gmail.com>
2023-03-16 15:11:31 +0800

    add izin

--- src/components/FormEditIzin.jsx
+++ src/components/FormEditIzin.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import { Container, Button, Card, Form, FloatingLabel } from "react-bootstrap";
+import { Container, Button, Card, Form, FloatingLabel, Alert, Spinner } from "react-bootstrap";
 import "bootstrap";
 
 const FormEditIzin = () => {
   const [ket, setKet] = useState("");
   const [msg, setMsg] = useState("");
+  const [psn, setPsn] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
 
@@ -26,6 +28,7 @@ const FormEditIzin = () => {
 
   const updateIzin = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       await axios.patch(`https://sizin-server.herokuapp.com/izin/${id}`, {
         ket: ket,
@@ -34,6 +37,8 @@ const FormEditIzin = () => {
     } catch (error) {
       if (error.response) {
         setMsg(error.response.data.msg);
+        setPsn(error.response.data.msg);
+        setLoading(false);
       }
     }
   };
@@ -49,10 +54,23 @@ const FormEditIzin = () => {
           <FloatingLabel controlId="floatingTextarea" label="Keterangan Izin" className="mb-3 w-100">
             <Form.Control as="textarea" className="input" value={ket} onChange={(e) => setKet(e.target.value)} placeholder="keterangan izin" />
           </FloatingLabel>
-          <Button type="submit" className="Button is-success w-100">
+          {loading === false ? (
+            <Button type="submit" onClick={() => setMsg("")} className="Button is-success w-100">
+              Simpan
+            </Button>
+          ) : (
+            <Button variant="primary" className="d-flex w-100 justify-content-center mt-4" disabled>
+              <Spinner as="span" animation="grow" size="sm" role="status" aria-hidden="true" />
+              {console.log(loading)}
+              Memproses...
+            </Button>
+          )}
+          {/* <Button type="submit" className="Button is-success w-100">
             Simpan
-          </Button>
-          <p className="has-text-centered">{msg}</p>
+          </Button> */}
+          <Alert className="d-flex flex-column align-items-center justify-content-center mt-2 w-100" variant={psn === "Cek kembali data anda!" ? "danger" : "Light"}>
+            {psn ? `${psn}` : "Masukkan keterangan izin"}
+          </Alert>
         </Form>
       </Card>
     </Container>
